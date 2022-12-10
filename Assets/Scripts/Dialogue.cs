3bb4a37d23e41fc952b842c230a1164f72283cd7
LeoRSousa <leorsousa@hotmail.com>
2022-12-09 21:06:12 -0300

    Lore pt. 01

--- Assets/Scripts/Dialogue.cs
+++ Assets/Scripts/Dialogue.cs
@@ -9,16 +9,55 @@ public class Dialogue : MonoBehaviour
     public TextMeshProUGUI textDialog;
     public string[] lines;
     public float textSpeed;
+
+    private int index;
     
     // Start is called before the first frame update
     void Start()
     {
-        
+        textDialog.text = string.Empty;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetMouseButtonDown(0))
+        {
+            if(textDialog.text == lines[index]) NextLine();
+        }
+        else
+        {
+            StopAllCoroutines();
+            textDialog.text = lines[index];
+        }
+    }
+
+    void StartDialogue()
+    {
+        index = 0;
+        StartCoroutine(TypeLine());
+    }
+
+    IEnumerator TypeLine()
+    {
+        foreach (char c in lines[index].ToCharArray())
+        {
+            textDialog.text += c;
+            yield return new WaitForSeconds(textSpeed);
+        }
+    }
+
+    void NextLine()
+    {
+        if (index < lines.Length - 1)
+        {
+            index++;
+            textDialog.text = string.Empty;
+            StartCoroutine(TypeLine());
+        }
+        else
+        {
+            gameObject.SetActive(false);
+        }
     }
 }
